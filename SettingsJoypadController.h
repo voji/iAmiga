77909fb69aab3949da825d546050cfaf74efafcb
emufreak <uersuschmid@me.com>
2015-05-14 22:03:14 +0200

    Start implementing of Gamecontrollersettings

---
+++ SettingsJoypadController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsJoypadController.h
+//  iUAE
+//
+//  Created by Urs on 14.05.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsJoypadController : UITableViewController
+
+@end
