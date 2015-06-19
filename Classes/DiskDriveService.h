d22eaa73c2c3c5418daa64ea90c16d9014249794
Simon Toens <simontoens@gmail.com>
2015-06-19 21:10:00 +0900

    Add DiskDriveService to project.

---
+++ Classes/DiskDriveService.h
@@ -0,0 +1,13 @@
+//
+//  DIskDriveService.h
+//  iUAE
+//
+//  Created by Simon Toens on 6/19/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DiskDriveService : NSObject
+
+@end
---
+++ Classes/DiskDriveService.mm
@@ -0,0 +1,13 @@
+//
+//  DIskDriveService.m
+//  iUAE
+//
+//  Created by Simon Toens on 6/19/15.
+//
+//
+
+#import "DiskDriveService.h"
+
+@implementation DIskDriveService
+
+@end
